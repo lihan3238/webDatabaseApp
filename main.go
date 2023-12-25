293bf6948709293292e3ee5be25d52b0eaf68180
lihan3238 <lihan3238@163.com>
2023-12-26 07:46:58 +0800

    1

--- main.go
+++ main.go
@@ -1,130 +1,302 @@
+// main.go
 package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// Database configuration
-const (
-	dbUser     = "root"
-	dbPassword = "123456"
-	dbName     = "movie"
-	dbHost     = "192.168.50.128:3306"
-)
-
-// Movie represents a movie record
+// Movie 表示电影实体结构
 type Movie struct {
-	Title  string
-	Rating float64
-	Tags   []string
+	Title    string    `json:"title"`
+	Rating   float64   `json:"rating"`
+	Tags     []string  `json:"tags"`
+	DateTime time.Time `json:"dateTime"`
 }
 
-// User represents a user record
-type User struct {
-	UserID int
-	Gender string
-	Name   string
-}
-
-// connectDB initializes a connection to the database
-// connectDB initializes a connection to the database
-func connectDB() (*sql.DB, error) {
-	// Replace 'localhost' with your database server's IP address or hostname
-	// and '3306' with your MySQL server's port if it's not the default.
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", dbUser, dbPassword, dbHost, dbName))
-	if err != nil {
-		println("Error connecting to the database: ", err.Error())
-		return nil, err
-	}
-	return db, nil
+// UserMovieInfo 表示用户电影信息实体结构
+type UserMovieInfo struct {
+	Movie   Movie    `json:"movie"`
+	TopTags []string `json:"topTags"`
 }
 
 func main() {
-	db, err := connectDB()
+	// 设置数据库连接信息
+	db, err := sql.Open("mysql", "root:qcwh2018@tcp(localhost:3306)/movie")
 	if err != nil {
-		log.Fatal("Error connecting to the database: ", err)
+		log.Fatal(err)
 	}
 	defer db.Close()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			t, _ := template.ParseFiles("search.html")
-			t.Execute(w, nil)
-		} else if r.Method == "POST" {
-			// Handle search here
-		}
+	// 测试数据库连接
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected to the database!")
+
+	// 设置静态文件目录
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+	router.Static("/static", "static") // 静态文件路径
+
+	// 设置路由规则
+	router.GET("/", func(c *gin.Context) {
+		ginHtml(c)
 	})
 
-	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-		// Here you will need to parse the input from the user and then execute the appropriate SQL queries.
-		// I will provide an example of searching movies by user ID.
-		userID := r.URL.Query().Get("userid")
-		if userID != "" {
-			// Execute query
-			rows, err := db.Query(`
-				SELECT m.title, r.rating
-				FROM ratings r
-				INNER JOIN movies m ON m.movieId = r.movieId
-				WHERE r.userId = ?
-				ORDER BY r.timestamp DESC`, userID)
+	router.POST("/", func(c *gin.Context) {
+		userID := c.PostForm("userId")
+		keyword := c.PostForm("keyword")
+		year := c.PostForm("year")
+		tag := c.PostForm("tag")
+		task := c.PostForm("task")
+
+		var result interface{}
+
+		switch task {
+		case "task_a":
+			result = searchTaskA(db, userID, keyword, year, tag)
+		case "task_b":
+			result = searchTaskB(db, year)
+		case "task_c":
+			result = searchTaskC(db, tag)
+		case "task_d":
+			result = searchTaskD(db, userID)
+		case "task_e":
+			minRating, err := strconv.ParseFloat(keyword, 64)
 			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
+				log.Println("Error converting keyword to float64:", err)
+				// 处理转换错误的情况，可以返回错误信息给前端
+				c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid rating format"})
 				return
 			}
-			defer rows.Close()
-
-			var movies []Movie
-			for rows.Next() {
-				var movie Movie
-				if err := rows.Scan(&movie.Title, &movie.Rating); err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}
-				// Fetch top 3 tags for each movie
-				tagRows, err := db.Query(`
-					SELECT gt.tag
-					FROM genomescores gs
-					INNER JOIN genometags gt ON gt.tagId = gs.tagId
-					WHERE gs.movieId = (SELECT movieId FROM movies WHERE title = ?)
-					ORDER BY gs.relevance DESC
-					LIMIT 3`, movie.Title)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}
-				defer tagRows.Close()
-
-				for tagRows.Next() {
-					var tag string
-					if err := tagRows.Scan(&tag); err != nil {
-						http.Error(w, err.Error(), http.StatusInternalServerError)
-						return
-					}
-					movie.Tags = append(movie.Tags, tag)
-				}
-
-				movies = append(movies, movie)
-			}
-
-			// Convert the movies slice to JSON
-			jsonData, err := json.Marshal(movies)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-
-			// Set the content type to application/json
-			w.Header().Set("Content-Type", "application/json")
-			w.Write(jsonData)
+			result = searchTaskE(db, userID, minRating)
 		}
+
+		c.JSON(http.StatusOK, gin.H{"result": result})
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router.Run(":8080")
+}
+
+func searchTaskA(db *sql.DB, userID, keyword, year, tag string) []UserMovieInfo {
+	rows, err := db.Query(`
+		SELECT m.title, r.rating, t.tag, FROM_UNIXTIME(r.timestamp) as dateTime
+		FROM ratings r
+		JOIN movies m ON r.movieId = m.movieId
+		LEFT JOIN tags t ON r.userId = t.userId AND r.movieId = t.movieId
+		WHERE r.userId = ?
+		ORDER BY r.timestamp DESC
+	`, userID)
+
+	if err != nil {
+		log.Println("Error executing query:", err)
+		return nil
+	}
+	defer rows.Close()
+
+	var userMovieInfos []UserMovieInfo
+
+	for rows.Next() {
+		var title, tag string
+		var rating float64
+		var dateTime time.Time
+
+		err := rows.Scan(&title, &rating, &tag, &dateTime)
+		if err != nil {
+			log.Println("Error scanning row:", err)
+			continue
+		}
+
+		userMovieInfo := UserMovieInfo{
+			Movie: Movie{
+				Title:    title,
+				Rating:   rating,
+				DateTime: dateTime,
+			},
+			TopTags: []string{tag},
+		}
+
+		userMovieInfos = append(userMovieInfos, userMovieInfo)
+	}
+
+	return userMovieInfos
+}
+func searchTaskB(db *sql.DB, year string) []Movie {
+	// 查询不同年代的电影并按受欢迎程度排序的示例代码
+	rows, err := db.Query(`
+		SELECT m.title, AVG(r.rating) as avgRating
+		FROM movies m
+		JOIN ratings r ON m.movieId = r.movieId
+		WHERE m.year = ?
+		GROUP BY m.title
+		ORDER BY avgRating DESC
+		LIMIT 20
+	`, year)
+
+	if err != nil {
+		log.Println("Error executing query:", err)
+		return nil
+	}
+	defer rows.Close()
+
+	var movies []Movie
+
+	for rows.Next() {
+		var title string
+		var avgRating float64
+
+		err := rows.Scan(&title, &avgRating)
+		if err != nil {
+			log.Println("Error scanning row:", err)
+			continue
+		}
+
+		movie := Movie{
+			Title:  title,
+			Rating: avgRating,
+		}
+
+		movies = append(movies, movie)
+	}
+
+	return movies
+}
+
+func searchTaskC(db *sql.DB, genre string) []Movie {
+	// 查询某一风格最受欢迎的20部电影的示例代码
+	rows, err := db.Query(`
+		SELECT m.title, AVG(r.rating) as avgRating
+		FROM movies m
+		JOIN ratings r ON m.movieId = r.movieId
+		WHERE FIND_IN_SET(?, m.genres) > 0
+		GROUP BY m.title
+		ORDER BY avgRating DESC
+		LIMIT 20
+	`, genre)
+
+	if err != nil {
+		log.Println("Error executing query:", err)
+		return nil
+	}
+	defer rows.Close()
+
+	var movies []Movie
+
+	for rows.Next() {
+		var title string
+		var avgRating float64
+
+		err := rows.Scan(&title, &avgRating)
+		if err != nil {
+			log.Println("Error scanning row:", err)
+			continue
+		}
+
+		movie := Movie{
+			Title:  title,
+			Rating: avgRating,
+		}
+
+		movies = append(movies, movie)
+	}
+
+	return movies
+}
+
+func searchTaskD(db *sql.DB, gender string) []Movie {
+	// 根据用户性别推荐最受欢迎的电影20部电影的示例代码
+	rows, err := db.Query(`
+		SELECT m.title, AVG(r.rating) as avgRating
+		FROM movies m
+		JOIN ratings r ON m.movieId = r.movieId
+		JOIN users u ON r.userId = u.userId
+		WHERE u.gender = ?
+		GROUP BY m.title
+		ORDER BY avgRating DESC
+		LIMIT 20
+	`, gender)
+
+	if err != nil {
+		log.Println("Error executing query:", err)
+		return nil
+	}
+	defer rows.Close()
+
+	var movies []Movie
+
+	for rows.Next() {
+		var title string
+		var avgRating float64
+
+		err := rows.Scan(&title, &avgRating)
+		if err != nil {
+			log.Println("Error scanning row:", err)
+			continue
+		}
+
+		movie := Movie{
+			Title:  title,
+			Rating: avgRating,
+		}
+
+		movies = append(movies, movie)
+	}
+
+	return movies
+}
+
+func searchTaskE(db *sql.DB, gender string, minRating float64) []Movie {
+	// 区分性别，查询高于某个评分的打分情况的示例代码
+	rows, err := db.Query(`
+		SELECT m.title, r.rating
+		FROM movies m
+		JOIN ratings r ON m.movieId = r.movieId
+		JOIN users u ON r.userId = u.userId
+		WHERE u.gender = ? AND r.rating > ?
+		ORDER BY r.rating DESC
+	`, gender, minRating)
+
+	if err != nil {
+		log.Println("Error executing query:", err)
+		return nil
+	}
+	defer rows.Close()
+
+	var movies []Movie
+
+	for rows.Next() {
+		var title string
+		var rating float64
+
+		err := rows.Scan(&title, &rating)
+		if err != nil {
+			log.Println("Error scanning row:", err)
+			continue
+		}
+
+		movie := Movie{
+			Title:  title,
+			Rating: rating,
+		}
+
+		movies = append(movies, movie)
+	}
+
+	return movies
+}
+
+func ginHtml(c *gin.Context) {
+	// 传递给 HTML 模板的数据
+	data := gin.H{"user_name": "lihan", "age": 32, "status": http.StatusOK, "data": gin.H{"id": 1, "name": "lihan"}}
+	c.HTML(http.StatusOK, "index.html", data)
 }
--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,46 @@
-function searchMovies() {
-    // 获取表单数据和执行查询逻辑
-    const userId = document.getElementById('userId').value;
-    const keyword = document.getElementById('keyword').value;
-    const year = document.getElementById('year').value;
-    const genre = document.getElementById('genre').value;
+// // 修改 searchMovies 函数
+// function searchMovies() {
+//     // 获取输入的用户ID
+//     var userId = document.getElementById("userId").value;
 
-    // 在这里添加你的查询逻辑和与后端交互的代码
-    // 使用fetch API或其他方式发送请求到后端
+//     // 创建 XMLHttpRequest 对象
+//     var xhr = new XMLHttpRequest();
 
-    // 模拟显示查询结果，实际中需要替换为从后端获取的数据
-    const resultsContainer = document.getElementById('results');
-    resultsContainer.innerHTML = `<p>显示查询结果的地方</p>`;
-}
+//     // 配置请求
+//     xhr.open("POST", "/", true);
+//     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+//     // 设置回调函数
+//     xhr.onreadystatechange = function () {
+//         if (xhr.readyState == 4 && xhr.status == 200) {
+//             // 处理返回的数据
+//             displayResults(xhr.responseText);
+//         }
+//     };
+
+//     // 构建请求参数
+//     var params = "userId=" + userId;
+
+//     // 发送请求
+//     xhr.send(params);
+// }
+
+// // 新增函数用于显示查询结果
+// function displayResults(data) {
+//     // 解析 JSON 数据
+//     var movies = JSON.parse(data);
+
+//     // 获取显示结果的容器
+//     var resultsContainer = document.getElementById("movieResults");
+
+//     // 清空之前的结果
+//     resultsContainer.innerHTML = "";
+
+//     // 遍历电影数据并添加到容器中
+//     for (var i = 0; i < movies.length; i++) {
+//         var movie = movies[i];
+//         var movieDiv = document.createElement("div");
+//         movieDiv.innerHTML = "Movie: " + movie.title + ", Rating: " + movie.rating + ", Tag: " + movie.tag;
+//         resultsContainer.appendChild(movieDiv);
+//     }
+// }
